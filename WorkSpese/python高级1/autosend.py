aaa97f9e550fd3faaf299d6456c6423759c87205
car <912979874@qq.com>
2019-08-17 18:32:19 +0800

    微信小脚本’

---
+++ WorkSpese/python高级1/autosend.py
@@ -0,0 +1,25 @@
+# encoding = utf8
+import itchat, time, re
+from itchat.content import *
+
+
+# 如果对方发的是文字，则我们给对方回复以下的东西
+@itchat.msg_register([TEXT])
+def text_reply(msg):
+    match = re.search('在', msg['Text'])
+    if match:
+        itchat.send(('[自动回复] 主人不在，稍等一下'), msg['FromUserName'])
+
+    # 如果对方发送的是图片，音频，视频和分享的东西我们都做出以下回复。
+
+
+@itchat.msg_register([PICTURE, RECORDING, VIDEO, SHARING])
+def other_reply(msg):
+    #想给谁发信息
+    users=itchat.search_friends(name='miki')
+    #找到username
+    userName=users[0]['UserName']
+    itchat.send('[自动回复] 主人%s给你发语音或者文件了'%name['FromUserName'],toUserName=userName)
+
+itchat.auto_login(hotReload=False)
+itchat.run()
